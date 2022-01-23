6c0413c24256b7abd1cdfd6429812853527b3655
Jindřich Kvita <jindrich.kvita@gnj.cz>
2022-01-23 22:02:51 +0100

    add cpp project

---
+++ projektcpp/cmake-build-debug/CMakeFiles/projektcpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projektcpp.dir/main.cpp.o"
+  "projektcpp"
+  "projektcpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projektcpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
